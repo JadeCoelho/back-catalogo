5c03e6fa9b9a91bd7c66ed51e2acfd18cbbdf272
Jade <jademfcoelho@gmail.com>
2024-03-27 17:58:38 -0300

    testando versoes node

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-// JSON Server module
-const jsonServer = require("json-server");
+const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-
-// Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8000; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
-// Add this before server.use(router)
-server.use(
-  // Add custom route here if needed
-  jsonServer.rewriter({
-    "/*": "/$1",
-  })
-);
 server.use(router);
-// Listen to port
 
-const port = process.env.PORT || 8000;
-
-server.listen(port, () => {
-  console.log("JSON Server is running");
-});
-
-// Export the Server API
-module.exports = server;
+server.listen(8000, function () {
+    console.log('JSON Server is running')
+  })
