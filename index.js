c45638a6551e4a40b3ed9e549286cb767ef196fe
Jade <jademfcoelho@gmail.com>
2024-03-28 08:52:28 -0300

    trocando com vercel

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const port = process.env.PORT || 8000; //  chose port from here like 8080, 3001
 server.use(middlewares);
 server.use(router);
 
-server.listen(8000, function () {
+server.listen(port, function () {
     console.log('JSON Server is running')
   })
